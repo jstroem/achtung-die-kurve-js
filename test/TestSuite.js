6349dbd7b15269ef62ea0fe50beae85c166a7c45
root <root@jstroem.com>
2011-09-12 23:04:36 +0400

    deleted:    .gitmodules 	modified:   node_modules/express 	modified:   test/TestSuite.js 	new file:   test/package.json

--- test/TestSuite.js
+++ test/TestSuite.js
@@ -4,3 +4,15 @@ var fs = require('fs');
 var folder = __dirname.substr(fs.realpathSync('.').length,__dirname.length-1);
 
 reporter.run([folder]);
+
+var express = require('express');
+var connect = require('connect');
+var app = express.createServer();
+// Routes
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(8991);
+
